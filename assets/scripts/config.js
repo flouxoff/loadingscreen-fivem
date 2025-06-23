65521a417b6c22256bccd8270bb62066edef37aa
FLOUX <56213627+flouxoff@users.noreply.github.com>
2025-06-23 19:12:13 +0200

    Add files via upload

---
+++ assets/scripts/config.js
@@ -0,0 +1,26 @@
+Config = {}; 
+
+Config.ServerIP = "YOUR IP SERVER";
+
+Config.Socials = [
+    {name: "discord", label: "Discord", description: "Click here to join our Discord server!", icon: "assets/media/icons/discord.png", link: "YOUR DISCORD LINK"},
+    {name: "instagram", label: "Instagram", description: "Click here to join our Instagram server!", icon: "assets/media/icons/tiktok.png", link: "YOUR INSTAGRAM LINK"},
+    {name: "tebex", label: "Donations", description: "Click here for give the donation!", icon: "assets/media/icons/tebex.png", link: "YOUR TEBEX LINK"},
+];
+
+Config.HideoverlayKeybind = 112
+Config.CustomBindText = "F1";
+
+Config.Staff = [
+    {name: "Owner #1", description: "Owner", color: "#d701fd", icon: "assets/media/icons/logo.png"},
+    {name: "Owner #2", description: "Owner", color: "#d701fd", icon: "assets/media/icons/logo.png"},
+
+
+];
+
+Config.Categories = [
+    {label: "Social Media", default: true},
+    {label: "Staff", default: false}
+];
+
+Config.Song = "song.mp3";
---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'Floux'
+description 'loadingscreen for FiveM'
+
+files {
+    '*.html',
+    'assets/**/*.*',
+    'assets/**/**/*.*'
+}
+
+client_script 'client.lua'
+
+loadscreen 'index.html'
